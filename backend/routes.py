17f1fa2e4108c7f36eec82cd2ce5d02c88e8c4d9
manohar <karnammanohar2405@gmail.com>
2026-06-28 15:09:03 +0530

    routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from werkzeug.utils import secure_filename
+import os
 
 api = Blueprint("api", __name__)
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+ALLOWED_EXTENSIONS = {"wav", "mp3","mpeg"}
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @api.route("/", methods=["GET"])
 def home():
@@ -10,8 +23,37 @@ def home():
         "mode": "Offline CPU AI"
     })
 
+
 @api.route("/health", methods=["GET"])
 def health():
     return jsonify({
         "status": "healthy"
     })
+
+
+@api.route("/upload", methods=["POST"])
+def upload_audio():
+
+    if "audio" not in request.files:
+        return jsonify({"error": "No audio file provided"}), 400
+
+    file = request.files["audio"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+
+        filename = secure_filename(file.filename)
+
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+        file.save(filepath)
+
+        return jsonify({
+            "message": "Upload Successful",
+            "filename": filename,
+            "path": filepath
+        })
+
+    return jsonify({"error": "Only .wav and .mp3 files are allowed"}), 400
